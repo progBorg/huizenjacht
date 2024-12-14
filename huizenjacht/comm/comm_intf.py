0a6aa1f0e24771c97afcfd9734affb95adb76d98
Tom Veldman <progborg@gmail.com>
2024-12-14 15:05:21 +0100

    Add basic Communication interface, start pushover implementation

--- huizenjacht/comm/comm_intf.py
+++ huizenjacht/comm/comm_intf.py
@@ -1,2 +1,21 @@
-class Comm:
-    pass
+from abc import ABC, abstractmethod
+
+class Comm(ABC):
+    """
+    Communication interface to update user.
+    """
+
+    """
+    Send a message to the user
+    """
+    @abstractmethod
+    def send(self, msg: str) -> int:
+        pass
+
+    """
+    Retrieve the readiness of this communication channel
+    """
+    @abstractmethod
+    def is_ready(self) -> bool:
+        pass
+    
--- huizenjacht/comm/pushover.py
+++ huizenjacht/comm/pushover.py
@@ -1,6 +1,14 @@
 from .comm_intf import Comm
+import logging
 
 class Pushover(Comm):
+    logger = logging.getLogger(__name__)
 
     def __init__(self):
+        pass
+
+    def send(self, msg: str) -> int:
+        pass
+
+    def is_ready(self) -> bool:
         pass
