b5a3f204d7bfc29e85c8b70750f07e0fb5b8da53
Tom <progborg@gmail.com>
2024-12-30 23:52:00 +0100

    Get Funda entries and add to database

--- huizenjacht/source/funda.py
+++ huizenjacht/source/funda.py
@@ -1,31 +1,139 @@
-from funda_scraper import FundaScraper
+import json
 import logging
+import requests
+import sqlite3
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 from huizenjacht.source import Source
 from huizenjacht.config import Config
 
 class Funda(Source):
+
+    # Constants
+    BASE_URL = "https://www.funda.nl/zoeken/"
+    _db_table_create_stmt = '''
+CREATE TABLE IF NOT EXISTS "Funda" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"URL"	TEXT UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+)'''
+
     # Public attributes
-    logger: logging.Logger
-    conf: dict
+    logger: logging.Logger = logging.getLogger(__name__)
+    conf: dict = None
+    db: sqlite3.Cursor
 
     # Private attributes
-    _FS: FundaScraper
+    _required_conf_entries: set = {
+        "area",
+        "buy_or_rent",
+    }
+    _req_url: str
+    _req_url_params: dict
+    _req_url_headers: dict
+    _ua: UserAgent
+    _conn: sqlite3.Connection
 
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
 
-        self.conf = Config().config['source']['funda']
+    def __init__(self, db: sqlite3.Connection):
+        self.conf = Config().config['sources']['funda']
+
         self._sanity_check_conf()
 
-    def get(self):
-        pass
+        self._setup_from_conf()
 
-    def is_new(self, house) -> bool:
-        pass
+        self._ua = UserAgent()
+
+        self._conn = db
+        self.db = db.cursor()
+
+        self.db.execute(self._db_table_create_stmt)
+
+    def get(self) -> list[str] | None:
+        soup = self._do_request()
+
+        if soup is None:
+            return []
+
+        url_list = self._parse_response(soup)
+
+        if url_list is None:
+            return []
+
+        return url_list
+
+    def _do_request(self) -> BeautifulSoup | None:
+        # Randomize User-Agent
+        headers = self._req_url_headers
+        headers["User-Agent"] = self._ua.random
+
+        # Do request
+        res = requests.get(
+            url=self._req_url,
+            params=self._req_url_params,
+            headers=headers,
+        )
+
+        if res.status_code != 200:
+            self.logger.warning("Could not reach Funda page successfully, http status code %i", res.status_code)
+            return None
+
+        return BeautifulSoup(res.text, features="html.parser")
+
+    def _parse_response(self, soup: BeautifulSoup) -> list:
+        # Get urls
+        urls_json = json.loads("".join(soup.find("script", {"type": "application/ld+json"}).contents[0]))
+        urls = [item["url"] for item in urls_json["itemListElement"]]
+
+        return urls
+
+    def is_new(self, house: str) -> bool:
+        try:
+            self.db.execute("insert into Funda (url) values (?)", [house])
+        except sqlite3.IntegrityError:
+            return False
+
+        return True
 
     def _sanity_check_conf(self):
-        pass
+        super()
+
+        buy_or_rent = self.conf_value("buy_or_rent")
+        if not buy_or_rent in ("buy", "koop", "rent", "huur"):
+            raise ValueError('Config entry buy_or_rent must be one of [buy, rent, koop, huur]')
+
+        logger.info("Configuration sanity check successful")
+
+    def _setup_from_conf(self):
+        # Build base url
+        buy_or_rent = self.conf_value("buy_or_rent")
+        url = urljoin(self.BASE_URL, "koop" if buy_or_rent in ("koop", "buy") else "huur")
+
+        # Get values and build url parameters
+        url_params = {
+            "selected_area": self.conf_value("area"),
+            "min_price": self.conf_value("min_price", None),
+            "max_price": self.conf_value("max_price", None),
+            "min_rooms": self.conf_value("min_rooms", None),
+            "max_rooms": self.conf_value("max_rooms", None),
+            "property_type": self.conf_value("property_type", "woonhuis"),
+            "sort_by": self.conf_value("sort_by", "date-down"),
+        }
+
+        if isinstance(url_params["selected_area"], str):
+            url_params["selected_area"] = f'["{url_params["selected_area"]}"]'
+        elif isinstance(url_params["selected_area"], list):
+            url_params["selected_area"] = '[' + ','.join(f'"{city}"' for city in url_params["selected_area"]) + ']'
+
+        # Filter out keys with None value
+        url_params = {k: v for k, v in url_params.items() if v is not None}
+
+        self._req_url = url
+        self._req_url_params = url_params
+        self._req_url_headers = {}
+
 
 if __name__ == "__main__":
     # Quick functionality tests for this class
@@ -33,13 +141,15 @@ if __name__ == "__main__":
 ---
 server:
   debug: true
+  db: "huizenjacht.db"
   message_defaults:
     title: "Nieuw huis gevonden"
 
 sources:
   funda:
     active: true
-    settings:
+    area: "gorinchem"
+    buy_or_rent: "rent"
 """
 
     logging.basicConfig()
@@ -47,6 +157,13 @@ sources:
     logger.setLevel(logging.INFO)
 
     Config().load_text(conf_text)
-    f = Funda()
+    conf = Config().config
+    db = sqlite3.connect(conf["server"]["db"])
+    f = Funda(db)
 
-    print(f.get())
+    houses = f.get()
+
+    for h in houses:
+        print(f.is_new(h))
+
+    f._conn.commit()
--- huizenjacht/source/source_intf.py
+++ huizenjacht/source/source_intf.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Any
 from abc import ABC, abstractmethod
 
@@ -6,6 +7,27 @@ class Source(ABC):
     """
     Interface for information sources.
     """
+
+    # Public attributes
+    @property
+    @abstractmethod
+    def logger(self) -> logging.Logger:
+        pass
+
+    @property
+    @abstractmethod
+    def conf(self) -> dict:
+        pass
+
+    # Private attributes
+    @property
+    @abstractmethod
+    def _required_conf_entries(self) -> set:
+        pass
+
+    @abstractmethod
+    def __init__(self):
+        pass
 
     """
 	Get a list of available houses
@@ -20,3 +42,39 @@ class Source(ABC):
     @abstractmethod
     def is_new(self, house: Any) -> bool:
         pass
+
+    """
+    Get a value from the config
+    """
+    def conf_value(self, key: str | tuple, default = None):
+        if self.conf is None:
+            raise ValueError("Attempted to read config value, but config was not loaded")
+
+        value = None
+        try:
+            if isinstance(key, str):
+                value = self.conf.get(key, default)
+            elif isinstance(key, tuple):
+                for k in key:
+                    value = self.conf.get(k, default)
+        except KeyError as ex:
+            self.logger.error("Key %s does not exist in config, no default value given", key)
+            raise ex
+
+        return value
+
+    """
+    Basic configuration sanity check to see if all required keys are present
+    """
+    def _sanity_check_conf(self):
+        req_entries = dict.fromkeys(self._required_conf_entries)
+        for name in req_entries.keys():
+            try:
+                value = self.conf[name]
+            except KeyError as e:
+                logging.error(f"Required entry \"{name}\" not found in configuration")
+                raise e
+
+            req_entries[name] = value
+
+        self.logger.debug(req_entries)
