31a39cd6b659ceb7fe9b91e1c4e919ebd3b35241
Tom Veldman <37376120+progBorg@users.noreply.github.com>
2025-01-09 10:02:07 +0100

    Fix logging bug in funda.py

--- huizenjacht/source/funda.py
+++ huizenjacht/source/funda.py
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS "Funda" (
             urls_json = json.loads("".join(soup.find("script", {"type": "application/ld+json"}).contents[0]))
             urls = [item["url"] for item in urls_json["itemListElement"]]
         except AttributeError as exc:
-            logger.info(f"Failed to retrieve Funda urls from query with parameters {self._req_url_params}")
+            self.logger.info(f"Failed to retrieve Funda urls from query with parameters {self._req_url_params}")
             urls = None
 
         return urls
